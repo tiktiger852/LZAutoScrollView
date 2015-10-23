e609684d1c8fbc01a55f725eea9e65dde7545cf1
mac <mac@clover.local>
2015-10-23 17:50:27 +0800

    Initial Commit

---
+++ LZAutoScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZAutoScrollView
+//
+//  Created by mac on 10/23/15.
+//  Copyright © 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZAutoScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZAutoScrollView
+//
+//  Created by mac on 10/23/15.
+//  Copyright © 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
